52ed8f8111633020b97ff919c1c46ddc76cdb03d
NoctriLi <96703319+NoctriLi@users.noreply.github.com>
2023-11-30 14:50:49 -0500

    fix: handle error when fetching pokemon beforerndm

--- app/components/RandomPokemon.tsx
+++ app/components/RandomPokemon.tsx
@@ -3,11 +3,14 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const RandomPokemon = ({ pokemon }: { pokemon: any }) => {
-  const [randomNum, setRandomPokemon] = useState(1);
+  const [randomNum, setRandomPokemon] = useState(-1);
 
   const maxNum = 1017; // 1017 is the last actual pokemon, the api has extra forms as entries
   const minNum = 1; // 1 is the first pokemon
 
+  ///This is so the random number is generated on page load
+  ///instead of being generated with the static page
+  ///useEffect prevents hydration errors resulting from timing issues
   useEffect(() => {
     const randomNumGenerator = () => {
       if (randomNum === 0) setRandomPokemon(1);
@@ -18,7 +21,7 @@ const RandomPokemon = ({ pokemon }: { pokemon: any }) => {
     };
     randomNumGenerator();
   }, []);
-
+  if (randomNum === -1) return null;
   const randomPokemon = pokemon.results[randomNum - 1];
   const name = randomPokemon.name;
   const num = randomNum;
